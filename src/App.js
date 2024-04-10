fa654c274885dfbbe1f68b874044d7852839c159
DisorderUK <felix.a.clemson@gmail.com>
2024-04-10 14:44:10 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter((currentValue) => currentValue + 1)
+    }, 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The time sponsored by accurist will be: {counter}
         </p>
         <a
           className="App-link"
