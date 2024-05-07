e8689734d74d3f05bb2e663cd9b5054abe5174a4
DisorderUK <felix.a.clemson@gmail.com>
2024-05-07 18:51:18 +0100

    incorrect Uri changed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const generateRandomString = (length) => {
   return values.reduce((acc, x) => acc + possible[x % possible.length], "");
 };
 
-const appUri = process.env.APP_URI || 'http://locahost:3000/';
+const appUri = process.env.APP_URI || 'http://localhost:3000/';
 
 const sha256 = async (plain) => {
   const encoder = new TextEncoder();
